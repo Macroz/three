9af1d3f90e4faf46286c2e51050f41e1b514b953
Markku Rontu <markku.rontu@iki.fi>
2022-04-22 20:00:14 +0300

    Initial commit

---
+++ jsm/main.module.js
@@ -0,0 +1,74 @@
+import * as THREE from 'three';
+
+import { OrbitControls } from '../jsm/controls/OrbitControls.js';
+import { GLTFLoader } from '../jsm/loaders/GLTFLoader.js';
+//import { RGBELoader } from './jsm/loaders/RGBELoader.js';
+
+let camera, scene, renderer;
+
+init();
+render();
+
+function init() {
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+
+  camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.25, 20);
+  camera.position.set(-1.8, 0.6, 2.7);
+
+  scene = new THREE.Scene();
+
+  const loader = new GLTFLoader();
+  loader.load('dirt.glb', function (gltf) {
+    scene.add(gltf.scene);
+    render();
+  });
+
+  const light = new THREE.AmbientLight(0x404040);
+  scene.add(light);
+
+  const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.6);
+  hemiLight.color.setHSL(0.6, 1, 0.6);
+  hemiLight.groundColor.setHSL(0.095, 1, 0.75);
+  hemiLight.position.set(0, 50, 0);
+  scene.add(hemiLight);
+
+  const dirLight = new THREE.DirectionalLight(0xffffff, 1);
+  dirLight.color.setHSL(0.1, 1, 0.95);
+  dirLight.position.set(-0.3, 1, 0.5);
+  dirLight.position.multiplyScalar(30);
+  scene.add(dirLight);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setClearColor(0xffffff, 1);
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  container.appendChild(renderer.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.addEventListener('change', render); // use if there is no animation loop
+  controls.minDistance = 2;
+  controls.maxDistance = 10;
+  controls.target.set(0, 0, -0.2);
+  controls.update();
+
+  window.addEventListener('resize', onWindowResize);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+
+  render();
+}
+
+//
+
+function render() {
+  renderer.render(scene, camera);
+}
